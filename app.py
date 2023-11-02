86b2d3a78f1c05e25e65d5f5cf5bc181cc97daec
Manuel Fernando Campos Cerda <mcampos.cerda@gmail.com>
2023-11-02 15:11:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from streamlit_authenticator import Authenticator
 
-st.title('Hello, Streamlit!')
+authenticator = Authenticator()
+
+username = st.text_input('Username')
+password = st.text_input('Password', type='password')
+
+if st.button('Login'):
+    if authenticator.check_credentials(username, password):
+        st.success('Logged in successfully')
+    else:
+        st.error('Invalid credentials')
